ecbcd49487d85a740f113d88d23dfadfab6950f5
Leon Staufer <leon@staufer.me>
2021-09-22 15:39:15 +0200

    add command line options for preprocessing and verbosity

--- README.md
+++ README.md
@@ -1,5 +1,10 @@
 # DM2583-Lab2
 
-Install required packages via pip install
+Install required packages via `pip install`.
 
-Run the main.py file.
+Run the `main.py` file. 
+
+Command line options include
+
+* `-v` for verbose output 
+* `-p` for choosing the preprocessing algorithm. Choices are `sklearn` (default) or `own`.
--- main.py
+++ main.py
@@ -1,7 +1,8 @@
+from argparse import ArgumentParser
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
-from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer, TfidfVectorizer
+from sklearn.feature_extraction.text import TfidfVectorizer
 from process_data import process_strings
 from sklearn.svm import SVC
 from sklearn.svm import LinearSVC
@@ -11,32 +12,33 @@ train = pd.read_csv("train.csv", dtype={"score": np.int32, "text": str})
 test = pd.read_csv("test.csv", dtype={"score": np.int32, "text": str})
 evaluation = pd.read_csv("evaluation.csv", dtype={"score": np.int32, "text": str})
 
-
-
 # choose type of data processing
-algorithm = input("Please choose the type of data processing. Choices are 'sklearn' or 'own'.\n").lower()
+parser = ArgumentParser("Demonstration of SVM in training, predicting, and evaluating data sets.")
+parser.add_argument("-v", "--verbose", action="store_true", help="Print additional logs.")
+parser.add_argument("-p", "--preprocessor", default="sklearn",
+                    help="Type of preprocessor to be used. Options are sklearn' or 'own'.")
+args = parser.parse_args()
+algorithm = args.preprocessor
+
+# preprocess data
 if algorithm == "own":
+    print("Preprocessing data with own algorithm...")
     train["text"] = process_strings(train)
     test["text"] = process_strings(test)
     evaluation["text"] = process_strings(evaluation)
     vec = TfidfVectorizer()
-elif algorithm == "sklearn":
+else:
+    print("Preprocessing data with SKlearn...")
     train["text"] = train["text"].str.strip().str.lower()
     test["text"] = test["text"].str.strip().str.lower()
     evaluation["text"] = evaluation["text"].str.strip().str.lower()
     vec = TfidfVectorizer(stop_words='english')
-else:
-    print("Invalid choice")
-    exit(1)
-
-
 
 # transform data
 print("Transforming data...")
 x_train = vec.fit_transform(train["text"]).toarray()
 x_test = vec.transform(test["text"]).toarray()
 x_eval = vec.transform(evaluation["text"]).toarray()
-
 
 # train model
 print("Training model...")
@@ -45,7 +47,7 @@ print("Training model...")
 
 # you could try SVC too (high-dimensional) but I found it is extremely slow to train
 # model = SVC(kernel="rbf", C=1, verbose=True)
-model = LinearSVC(C=1, verbose=True)
+model = LinearSVC(C=1, verbose=args.verbose)
 model.fit(x_train, train["score"].values)
 
 # measure accuracies of models
@@ -63,8 +65,10 @@ test_data = evaluation
 test_vec = x_eval
 count0 = np.size(np.where(test_data["score"] == 0))
 count1 = np.size(np.where(test_data["score"] == 1))
-correct0 = np.size(np.where(np.logical_and(model.predict(test_vec) == test_data["score"].values, test_data["score"].values == 0)))
-correct1 = np.size(np.where(np.logical_and(model.predict(test_vec) == test_data["score"].values, test_data["score"].values == 1)))
+correct0 = np.size(
+    np.where(np.logical_and(model.predict(test_vec) == test_data["score"].values, test_data["score"].values == 0)))
+correct1 = np.size(
+    np.where(np.logical_and(model.predict(test_vec) == test_data["score"].values, test_data["score"].values == 1)))
 
 # confusion matrix for sentiment
 fig, ax = plt.subplots()
